ec277718ba20a79413f81a34f2f8ce42df16f705
iannolon <35769152+iannolon@users.noreply.github.com>
2018-05-30 08:25:17 -0400

    Updated from Brython Server: 5/30/2018 8:25:17 AM

--- mancala.py
+++ mancala.py
@@ -3,11 +3,11 @@
 #mancala.py
 
 from ggame import *
+#ADD THE DICTIONARY TO YLEN, STORES AND MATRIX
 
 def fillBuckets():
     matrix = [[4,4,4,4,4,4],[4,4,4,4,4,4]]
     stores = [0,0]
-    
 
 def redrawAll():
     for item in App().spritelist[:]:
@@ -25,53 +25,53 @@ def redrawAll():
     blackOutline = LineStyle(1,black)
     tanOutline = LineStyle(1,tan)
     
-    board = RectangleAsset(XLEN,YLEN,blackOutline,tan)
-    store = RectangleAsset(XLEN/7,YLEN/1.5,blackOutline,yellow)
-    house = CircleAsset(XLEN/25,blackOutline,blue) 
+    board = RectangleAsset(data['XLEN'],data['YLEN'],blackOutline,tan)
+    store = RectangleAsset(data['XLEN']/7,data['YLEN']/1.5,blackOutline,yellow)
+    house = CircleAsset(data['XLEN']/25,blackOutline,blue) 
     Sprite(board)
-    Sprite(store,(XLEN/18,YLEN/6))
-    Sprite(store,(XLEN/18+XLEN/1.35,YLEN/6))
-    Sprite(house,(2 * XLEN/10 +0.01*XLEN,YLEN/6))
-    Sprite(house,(3 * XLEN/10 +0.005*XLEN,YLEN/6))
-    Sprite(house,(4 * XLEN/10,YLEN/6))
-    Sprite(house,(5 * XLEN/10,YLEN/6))
-    Sprite(house,(6 * XLEN/10,YLEN/6))
-    Sprite(house,(7 * XLEN/10,YLEN/6))
-    Sprite(house,(2 * XLEN/10 +0.01*XLEN,YLEN/1.75))
-    Sprite(house,(3 * XLEN/10 +0.005*XLEN,YLEN/1.75))
-    Sprite(house,(4 * XLEN/10,YLEN/1.75))
-    Sprite(house,(5 * XLEN/10,YLEN/1.75))
-    Sprite(house,(6 * XLEN/10,YLEN/1.75))
-    Sprite(house,(7 * XLEN/10,YLEN/1.75))
+    Sprite(store,(data['XLEN']/18,data['YLEN']/6))
+    Sprite(store,(data['XLEN']/18+data['XLEN']/1.35,data['YLEN']/6))
+    Sprite(house,(2 * data['XLEN']/10 +0.01*data['XLEN'],data['YLEN']/6))
+    Sprite(house,(3 * data['XLEN']/10 +0.005*data['XLEN'],data['YLEN']/6))
+    Sprite(house,(4 * data['XLEN']/10,data['YLEN']/6))
+    Sprite(house,(5 * data['XLEN']/10,data['YLEN']/6))
+    Sprite(house,(6 * data['XLEN']/10,data['YLEN']/6))
+    Sprite(house,(7 * data['XLEN']/10,data['YLEN']/6))
+    Sprite(house,(2 * data['XLEN']/10 +0.01*data['XLEN'],data['YLEN']/1.75))
+    Sprite(house,(3 * data['XLEN']/10 +0.005*data['XLEN'],data['YLEN']/1.75))
+    Sprite(house,(4 * data['XLEN']/10,data['YLEN']/1.75))
+    Sprite(house,(5 * data['XLEN']/10,data['YLEN']/1.75))
+    Sprite(house,(6 * data['XLEN']/10,data['YLEN']/1.75))
+    Sprite(house,(7 * data['XLEN']/10,data['YLEN']/1.75))
     for r in range(0,6):
         for c in range(0,2):
             houNum = TextAsset(str(matrix[c][r-1]), fill = black, style = 'bold 50pt Times') #the font size does not change when the XLEN constant changes.
             if c == 0: #sprites first collumn
                 if r == 0:
-                    Sprite(houNum,((2+r)*XLEN/10+XLEN/50+XLEN*0.01,YLEN/6))
+                    Sprite(houNum,((2+r)*data['XLEN']/10+data['XLEN']/50+data['XLEN']*0.01,data['YLEN']/6))
                 elif r == 1:
-                    Sprite(houNum,((2+r)*XLEN/10+XLEN/50+XLEN*0.005,YLEN/6))
+                    Sprite(houNum,((2+r)*data['XLEN']/10+data['XLEN']/50+data['XLEN']*0.005,data['YLEN']/6))
                 else:
-                    Sprite(houNum,((2+r)*XLEN/10+XLEN/50,YLEN/6))
+                    Sprite(houNum,((2+r)*data['XLEN']/10+data['XLEN']/50,data['YLEN']/6))
             else: #sprites second collumn
                 if r == 0:
-                    Sprite(houNum,((2+r)*XLEN/10+XLEN/50+XLEN*0.01,YLEN/1.75))
+                    Sprite(houNum,((2+r)*data['XLEN']/10+data['XLEN']/50+data['XLEN']*0.01,data['YLEN']/1.75))
                 elif r == 1:
-                    Sprite(houNum,((2+r)*XLEN/10+XLEN/50+XLEN*0.005,YLEN/1.75))
+                    Sprite(houNum,((2+r)*data['XLEN']/10+data['XLEN']/50+data['XLEN']*0.005,data['YLEN']/1.75))
                 else:
-                    Sprite(houNum,((2+r)*XLEN/10+XLEN/50,YLEN/1.75))
+                    Sprite(houNum,((2+r)*data['XLEN']/10+data['XLEN']/50,data['YLEN']/1.75))
     for w in range(0,2):
         stoNum = TextAsset(str(stores[w]), fill = black, style = 'bold 80pt Times')
         if w == 0:
             if stores[0] < 10:
-                Sprite(stoNum,(XLEN/18+XLEN/25,YLEN/3))
+                Sprite(stoNum,(data['XLEN']/18+data['XLEN']/25,data['YLEN']/3))
             else:
-                Sprite(stoNum,(XLEN/18+XLEN/50,YLEN/3))
+                Sprite(stoNum,(data['XLEN']/18+data['XLEN']/50,data['YLEN']/3))
         else:
             if stores[1] < 10:
-                Sprite(stoNum,(XLEN/18+XLEN/25+XLEN/1.35,YLEN/3))
+                Sprite(stoNum,(data['XLEN']/18+data['XLEN']/25+data['XLEN']/1.35,data['YLEN']/3))
             else:
-                Sprite(stoNum,(XLEN/18+XLEN/50+XLEN/1.35,YLEN/3))
+                Sprite(stoNum,(data['XLEN']/18+data['XLEN']/50+data['XLEN']/1.35,data['YLEN']/3))
     
 
 def drawPieces():
@@ -81,30 +81,30 @@ def drawPieces():
             houNum = TextAsset(str(matrix[c][r-1]), fill = black, style = 'bold 50pt Times') #the font size does not change when the XLEN constant changes.
             if c == 0: #sprites first collumn
                 if r == 0:
-                    Sprite(houNum,((2+r)*XLEN/10+XLEN/50+XLEN*0.01,YLEN/6))
+                    Sprite(houNum,((2+r)*data['XLEN']/10+data['XLEN']/50+data['XLEN']*0.01,data['YLEN']/6))
                 elif r == 1:
-                    Sprite(houNum,((2+r)*XLEN/10+XLEN/50+XLEN*0.005,YLEN/6))
+                    Sprite(houNum,((2+r)*data['XLEN']/10+data['XLEN']/50+data['XLEN']*0.005,data['YLEN']/6))
                 else:
-                    Sprite(houNum,((2+r)*XLEN/10+XLEN/50,YLEN/6))
+                    Sprite(houNum,((2+r)*data['XLEN']/10+data['XLEN']/50,data['YLEN']/6))
             else: #sprites second collumn
                 if r == 0:
-                    Sprite(houNum,((2+r)*XLEN/10+XLEN/50+XLEN*0.01,YLEN/1.75))
+                    Sprite(houNum,((2+r)*data['XLEN']/10+data['XLEN']/50+data['XLEN']*0.01,data['YLEN']/1.75))
                 elif r == 1:
-                    Sprite(houNum,((2+r)*XLEN/10+XLEN/50+XLEN*0.005,YLEN/1.75))
+                    Sprite(houNum,((2+r)*data['XLEN']/10+data['XLEN']/50+data['XLEN']*0.005,data['YLEN']/1.75))
                 else:
-                    Sprite(houNum,((2+r)*XLEN/10+XLEN/50,YLEN/1.75))
+                    Sprite(houNum,((2+r)*data['XLEN']/10+data['XLEN']/50,data['YLEN']/1.75))
     for w in range(0,2):
         stoNum = TextAsset(str(stores[w]), fill = black, style = 'bold 80pt Times')
         if w == 0:
             if stores[0] < 10:
-                Sprite(stoNum,(XLEN/18+XLEN/25,YLEN/3))
+                Sprite(stoNum,(data['XLEN']/18+data['XLEN']/25,data['YLEN']/3))
             else:
-                Sprite(stoNum,(XLEN/18+XLEN/50,YLEN/3))
+                Sprite(stoNum,(data['XLEN']/18+data['XLEN']/50,data['YLEN']/3))
         else:
             if stores[1] < 10:
-                Sprite(stoNum,(XLEN/18+XLEN/25+XLEN/1.35,YLEN/3))
+                Sprite(stoNum,(data['XLEN']/18+data['XLEN']/25+data['XLEN']/1.35,data['YLEN']/3))
             else:
-                Sprite(stoNum,(XLEN/18+XLEN/50+XLEN/1.35,YLEN/3))
+                Sprite(stoNum,(data['XLEN']/18+data['XLEN']/50+data['XLEN']/1.35,data['YLEN']/3))
     
 '''
 def movePieces(row,collumn):
@@ -122,15 +122,28 @@ def gameOver():
         return False
     
 
-def mouseClick(event):
+#def mouseClick(event):
     #event.x and event.y
     '''
-    
+    2 * XLEN/10 +0.01*XLEN,YLEN/6
+    3 * XLEN/10 +0.005*XLEN,YLEN/6
+    Sprite(house,(4 * XLEN/10,YLEN/6
+    Sprite(house,(5 * XLEN/10,YLEN/6
+    Sprite(house,(6 * XLEN/10,YLEN/6
+    Sprite(house,(7 * XLEN/10,YLEN/6
+    Sprite(house,(2 * XLEN/10 +0.01*XLEN,YLEN/1.75
+    3 * XLEN/10 +0.005*XLEN,YLEN/1.75
+    4 * XLEN/10,YLEN/1.75
+    5 * XLEN/10,YLEN/1.75
+    6 * XLEN/10,YLEN/1.75
+    7 * XLEN/10,YLEN/1.75
+    '''
 
 
 if __name__ == '__main__':
-    XLEN = 975 #975 looks nice
-    YLEN = XLEN * 7/18 #To make the proportions the same
+    data = {}
+    data['XLEN'] = 975 #975 looks nice
+    data['YLEN'] = data['XLEN'] * 7/18 #To make the proportions the same
     fillBuckets()
     redrawAll()
     #matrix = [[4,4,2,4,7,4],[4,4,0,4,2,4]]
