82d85fe75d8deb3b8a4ec7208861541901d92907
iannolon <35769152+iannolon@users.noreply.github.com>
2018-05-30 08:09:32 -0400

    Updated from Brython Server: 5/30/2018 8:09:32 AM

--- mancala.py
+++ mancala.py
@@ -13,7 +13,9 @@ def redrawAll():
     for item in App().spritelist[:]:
         item.destroy()
     
-    fillBuckets()
+    #fillBuckets()
+    matrix = [[4,4,4,4,4,4],[4,4,4,4,4,4]]
+    stores = [0,0]
     
     black = Color(0x000000,1)
     tan = Color(0xE3E385,1)
@@ -22,9 +24,6 @@ def redrawAll():
     
     blackOutline = LineStyle(1,black)
     tanOutline = LineStyle(1,tan)
-    
-    XLEN = 975 #975 looks nice
-    YLEN = XLEN * 7/18 #To make the proportions the same
     
     board = RectangleAsset(XLEN,YLEN,blackOutline,tan)
     store = RectangleAsset(XLEN/7,YLEN/1.5,blackOutline,yellow)
@@ -74,11 +73,40 @@ def redrawAll():
             else:
                 Sprite(stoNum,(XLEN/18+XLEN/50+XLEN/1.35,YLEN/3))
     
-'''
-def drawPieces():
-    for stor in board:
-        print(stor)
 
+def drawPieces():
+    for r in range(0,6):
+        for c in range(0,2):
+            houNum = TextAsset(str(matrix[c][r-1]), fill = black, style = 'bold 50pt Times') #the font size does not change when the XLEN constant changes.
+            if c == 0: #sprites first collumn
+                if r == 0:
+                    Sprite(houNum,((2+r)*XLEN/10+XLEN/50+XLEN*0.01,YLEN/6))
+                elif r == 1:
+                    Sprite(houNum,((2+r)*XLEN/10+XLEN/50+XLEN*0.005,YLEN/6))
+                else:
+                    Sprite(houNum,((2+r)*XLEN/10+XLEN/50,YLEN/6))
+            else: #sprites second collumn
+                if r == 0:
+                    Sprite(houNum,((2+r)*XLEN/10+XLEN/50+XLEN*0.01,YLEN/1.75))
+                elif r == 1:
+                    Sprite(houNum,((2+r)*XLEN/10+XLEN/50+XLEN*0.005,YLEN/1.75))
+                else:
+                    Sprite(houNum,((2+r)*XLEN/10+XLEN/50,YLEN/1.75))
+    for w in range(0,2):
+        stoNum = TextAsset(str(stores[w]), fill = black, style = 'bold 80pt Times')
+        if w == 0:
+            if stores[0] < 10:
+                Sprite(stoNum,(XLEN/18+XLEN/25,YLEN/3))
+            else:
+                Sprite(stoNum,(XLEN/18+XLEN/50,YLEN/3))
+        else:
+            if stores[1] < 10:
+                Sprite(stoNum,(XLEN/18+XLEN/25+XLEN/1.35,YLEN/3))
+            else:
+                Sprite(stoNum,(XLEN/18+XLEN/50+XLEN/1.35,YLEN/3))
+    
+
+'''
 def movePieces(row,collumn):
     for i in range(board[row][collumn]):
         
@@ -91,6 +119,12 @@ def mouseClick(event):
 
 
 if __name__ == '__main__':
+    XLEN = 975 #975 looks nice
+    YLEN = XLEN * 7/18 #To make the proportions the same
+    fillBuckets()
     redrawAll()
+    matrix = [[4,4,2,4,7,4],[4,4,0,4,2,4]]
+    stores = [0,6]
+    drawPieces()
     App().run()
 
